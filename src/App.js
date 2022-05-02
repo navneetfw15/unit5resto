71a9df44a7a7a2a8133bc00923cef8c2fdebc538
navneetfw15 <navneet638864@gmail.com>
2022-05-02 21:39:53 +0530

    text file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Input } from "./components/restroInput"
+import { Mapping } from './components/Mapping';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+  const [show ,setShow]=useState(true);
+return (<div className='App'>
+
+{show ?<Mapping/> :<Input/>}
+<button className='show'
+onClick={()=>{
+  setShow(!show)
+}}
+>{show?"Add Another Restorent":"Restrorent Data"}</button>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/restoInput.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import css from "./Input.css";
+
+
+export const Input =()=>{
+const [Data,setData]=useState({
+    Name:"",
+    Title:"",
+    Cost:+(""),
+    Card:"",
+    Votes: +(""),
+    Review:+(""),
+    Rating:+(""),
+    Image:"",
+    Loved:+("")
+});
+function handleChange(e){
+const {id,value}=e.target;
+
+setData({
+    ...Data,
+    [id]:value
+})
+
+}
+
+function handleSubmit(event){
+event.preventDefault();
+
+fetch ("http://localhost:8080/restro",{
+method:"POST",
+headers:{
+    "content-type":"application/json"
+}
+,
+body:JSON.stringify(Data)
+
+
+})
+
+
+}
+
+    return (<div className="Input">
+
+<form onSubmit={handleSubmit}>
+    
+<input id="Name" onChange={handleChange} placeholder="Resto Name " type="text"/>
+<br/>
+<input id="Title" onChange={handleChange} placeholder="Dish Name " type="text"/>
+<br/>
+ <input id="Cost" onChange={handleChange} placeholder="Cost" type="number" />
+<br/>
+<input id="Card" onChange={handleChange} placeholder="Card Accepted " type="text" />
+<br/>
+<input id="Votes" onChange={handleChange}  placeholder="Votes" type="number" />
+<br/>
+<input id="Review" onChange={handleChange} placeholder="Review" type="number" />
+<br/>
+<input id="Rating" onChange={handleChange} type="number" placeholder="Rating" />
+<br/>
+<input onChange={handleChange} id="Loved" type="number" placeholder="Loved For People" />
+<br/>
+<input id="Image" onChange={handleChange} placeholder="mage Url " type="text"/>
+<br/>
+
+<input className="button" type="submit" value="Submit"/>
+</form>
+
+    </div>)
+}
